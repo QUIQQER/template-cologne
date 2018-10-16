16c738ca57ea211c8e435544bf8c4b02aa8f43a0
michael.daniel <michael@pcsg.de>
2018-10-16 15:42:17 +0200

    feat: Sticky top navigation. refactor: Translation improved.

--- bin/javascript/init.js
+++ bin/javascript/init.js
@@ -116,6 +116,11 @@ window.addEvent('domready', function () {
 
                     showBasketBegin: function (Basket, pos) {
 
+                        var bPosition = Basket.getElm().offsetTop,
+                            bHeight = Basket.getElm().getHeight();
+//                        console.log(Basket.getElm().offsetTop)
+//                        console.log(bHeight)
+
                         var winSize    = window.getSize(),
                             basketSize = Basket.getElm().getSize();
 
@@ -123,12 +128,29 @@ window.addEvent('domready', function () {
 //                        pos.x = pos.x - 500;
                         // muss noch besser gemacht werden
                         pos.x = winSize.x - 350 - 20;
-                        pos.y = pos.y + 10;
+//                        pos.y = pos.y + 10;
+//                        pos.y = bPosition  + bHeight + 12;
+
+
+
+                        /*neu*/
+                        var Header = document.getElement('.cologne-header');
+                        console.log(Header.getPosition().y)
+//                        console.log(Basket.getElm().getCoordinates(Header))
+console.log(pos.y)
+                        pos.y = Header.getPosition().y;
+
+                        Basket.$BasketContainer.setStyles({
+                            position: 'fixed'
+                        })
+
 
 
                         Basket.$BasketContainer.setStyles({
                             border: '1px solid #ddd'
                         });
+
+                        console.log(document.getElement('.quiqqer-order-basket-small-container').getPosition());
                     }
                 }
             }).inject(document.getElement('.cologne-header-control-basket'));
@@ -141,5 +163,39 @@ window.addEvent('domready', function () {
         require(['package/quiqqer/currency/bin/controls/Switch'], function (Switch) {
             new Switch().inject(document.getElement('.cologne-header-control-currencies'));
         });
+
+
+        /**
+         * Sticky menu
+         * @type {boolean}
+         */
+        var menuFixed    = false,
+            Menu         = document.getElement('.cologne-header'),
+            menuHeight   = Menu.getSize().y,
+            topBarHeight = document.getElement('.topbar').getSize().y;
+
+        var setMenuFixed = function () {
+            Menu.addClass('cologne-header-fixed');
+            document.getElement('body').setStyle('padding-top', menuHeight);
+            menuFixed = true;
+        };
+
+        var removeMenuFixed = function () {
+            Menu.removeClass('cologne-header-fixed');
+            document.getElement('body').setStyle('padding-top', null);
+            menuFixed = false;
+        };
+
+        if (Menu) {
+            window.addEvent('scroll', function () {
+
+                if (QUI.getScroll().y >= topBarHeight) {
+                    setMenuFixed();
+                    return;
+                }
+
+                removeMenuFixed();
+            })
+        }
     });
 });
