def9bd8716ed8c2228fef7e6b9623afdfd7c2078
michael.daniel <michael@pcsg.de>
2018-12-03 14:42:42 +0100

    feat: Frontend user profil on top of category menu. refactor: Folder Images to Image renamed.

---
+++ src/QUI/TemplateCologne/Utils.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file contains QUI\TemplateCologne\Utils
+ */
+
+namespace QUI\TemplateCologne;
+
+use QUI;
+
+/**
+ * Class Utils
+ */
+class Utils
+{
+    /**
+     * Get user avatar. If no avatar available return false.
+     *
+     * @param $User QUI\Interfaces\Users\User
+     * @throws QUI\Exception
+     *
+     * @return QUI\Projects\Media\Image|false
+     */
+    public static function getAvatar($User)
+    {
+        if (!$User instanceof QUI\Interfaces\Users\User) {
+            throw new QUI\Exception([
+                QUI::getLocale()->get(
+                    'quiqqer/template-cologne', 'exception.user.required'
+                )
+            ]);
+        }
+
+        $result = QUI::getEvents()->fireEvent('userGetAvatar', [$User]);
+
+        foreach ($result as $Entry) {
+            if ($Entry instanceof QUI\Interfaces\Projects\Media\File) {
+                return $Entry;
+            }
+        }
+
+        $avatar = $User->getAttribute('avatar');
+
+        if (!QUI\Projects\Media\Utils::isMediaUrl($avatar)) {
+            return false;
+        }
+
+        try {
+            return QUI\Projects\Media\Utils::getImageByUrl($avatar);
+        } catch (QUI\Exception $Exception) {
+        }
+
+        return false;
+    }
+}
