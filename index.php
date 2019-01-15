4e5f9be335130ddff2153ad8cc86fa25118d51e1
michael.daniel <michael@pcsg.de>
2019-01-15 13:45:40 +0100

    feat: pageHeader own template. feat: Spacing optimalized between content and bricks (grid, template-grid, template-row). feat: 2 from 4 layouts adapted. feat: Flexbox to manage left and right sidebar.

--- index.php
+++ index.php
@@ -21,20 +21,30 @@ $Avatar = new QUI\FrontendUsers\Controls\UserIcon([
     'User'       => QUI::getUserBySession()
 ]);
 
+/* product page - for layouts */
+$productPage = false;
+switch ($Site->getAttribute('type')) {
+    case 'quiqqer/products:types/category':
+    case 'quiqqer/products:types/search':
+        $productPage = true;
+
+};
+
 $Engine->assign([
     'BricksManager' => QUI\Bricks\Manager::init(),
     'Project'       => $Project,
     'Menu'          => $Menu,
-    'Avatar'        => $Avatar
+    'Avatar'        => $Avatar,
+    'productPage'   => $productPage
 ]);
 
 /**
  * Template config
  */
-$templateSettings = QUI\TemplateCologne\Utils::getConfig(array(
+$templateSettings = QUI\TemplateCologne\Utils::getConfig([
     'Project'  => $Project,
     'Site'     => $Site,
     'Template' => $Template
-));
+]);
 
 $Engine->assign($templateSettings);
