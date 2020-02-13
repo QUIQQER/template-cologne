885904772bcf3624efca5c00c2ce5e18039a3e6e
michael.danielczok <michael@pcsg.de>
2020-02-13 08:33:39 +0100

    refactor: Own smarty fetch method deleted.

--- src/QUI/TemplateCologne/EventHandler.php
+++ src/QUI/TemplateCologne/EventHandler.php
@@ -43,39 +43,6 @@ class EventHandler
     }
 
     /**
-     * Event : on smarty init
-     * @param \Smarty $Smarty - \Smarty
-     */
-    public static function onSmartyInit($Smarty)
-    {
-        if (!isset($Smarty->registered_plugins['function']) ||
-            !isset($Smarty->registered_plugins['function']['fetch'])
-        ) {
-            $Smarty->registerPlugin(
-                "function",
-                "fetch",
-                "\\QUI\\TemplateCologne\\EventHandler::fetch"
-            );
-        }
-    }
-
-    /**
-     * @param $params
-     * @param $Smarty
-     * @return string
-     */
-    public static function fetch($params, $Smarty)
-    {
-        $template = $params['template'];
-        $path     = OPT_DIR.'quiqqer/template-cologne/';
-
-        $Engine = QUI::getTemplateManager()->getEngine();
-        $Engine->assign($params);
-
-        return $Engine->fetch($path.$template);
-    }
-
-    /**
      * @param \Quiqqer\Engine\Collector $Collector
      */
     public static function onQuiqqerProductsProductButtonsEnd(\Quiqqer\Engine\Collector $Collector)
